689356a48bf6840d0a5286f26aab1eb528e3e6fb
Jamshidbek <detective.sobirov@gmail.com>
2024-03-19 21:29:43 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+"use strict"
+
+// alert("Hello, JavaScript")
+
+// const place = confirm("Are you here?")
+// console.log(place)
+
+// const currentPlace = +prompt("Where are you?", "")
+// console.log(typeof currentPlace)
+
+// const favoriteColor = []
+// favoriteColor[0] = prompt("What's your favourite color #1", "")
+// favoriteColor[1] = prompt("What's your favourite color #2", "")
+// favoriteColor[2] = prompt("What's your favourite color #3", "")
+// console.log(typeof favoriteColor)4
+
+// const channel = prompt("What's your favourite youtuber?")
+// console.log("https://youtube.com/" + channel)
