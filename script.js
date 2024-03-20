b78cfe781eb51770e48dbaf96f779802fc7ba050
Jamshidbek <detective.sobirov@gmail.com>
2024-03-20 11:52:44 +0500

    New code

--- script.js
+++ script.js
@@ -16,3 +16,68 @@
 
 // const channel = prompt("What's your favourite youtuber?")
 // console.log("https://youtube.com/" + channel)
+
+
+// const numberOfSeries = +prompt("Nechta o'yin ko'rdingiz?", "") 
+// console.log(numberOfSeries)
+
+// seriesDB = {
+//     count: numberOfSeries,
+//     series: {},
+//     actors: {},
+//     genres: [],
+//     private: false
+// }
+
+
+
+// const age = +prompt("How old are you?", "")
+
+// const color = "yellow"
+
+// switch (color) {
+//     case "red": 
+//         console.log("Stop!");
+//         break;
+//     case "green":
+//         console.log("Goo!");
+//         break;
+//     case "yellow":
+//         console.log("Slowly!");
+//         break;
+//     default:
+//         console.log("Traffic jam!");
+//         break;
+// }
+
+let num = 10;
+
+// while(startNum <= 10) {
+//     console.log(startNum) 
+//     startNum ++
+// }
+
+// do {
+//     console.log(startNum)
+//     startNum ++
+// } while (startNum <= 10)
+
+const seriesDB = {
+    series: {}
+}
+
+
+for (let i = 1; i <= 2; i++) {
+    const nameOfSeries = prompt("Oxirgi ko'rgan serialingiz", "")
+    const rankOfSeries = +prompt("Nechchi baho berasiz?", "")
+    
+    if (nameOfSeries != null && rankOfSeries != null && nameOfSeries != "" && rankOfSeries != "") {
+        seriesDB.series[nameOfSeries] = rankOfSeries
+        console.log("Done")
+    } else {
+        console.log("Error")
+        i--
+    }
+}
+
+console.log(seriesDB)
